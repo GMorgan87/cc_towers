b70cde49a63238018b88996d3ed33796313ccfe0
FKeir <fmkeir@gmail.com>
2020-03-25 13:56:18 +0000

    Create Bedroom with getters/count guests

---
+++ src/main/java/Bedroom.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Bedroom {
+
+    private int roomNumber;
+    private int capacity;
+    private String type;
+    private ArrayList<Guest> guests;
+
+    public Bedroom(int roomNumber, int capacity, String type) {
+        this.roomNumber = roomNumber;
+        this.capacity = capacity;
+        this.type = type;
+        this.guests = new ArrayList<Guest>();
+    }
+
+    public int countGuests() {
+        return this.guests.size();
+    }
+
+    public int getRoomNumber(){
+        return this.roomNumber;
+    }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+}
--- src/test/java/BedroomTest.java
+++ src/test/java/BedroomTest.java
@@ -1,4 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class BedroomTest {
 
-privateBedroom bedroom;
+    private Bedroom bedroom;
+
+    @Before
+    public void before(){
+        bedroom = new Bedroom(5, 2, "double");
+    }
+
+    @Test
+    public void guestsStartsEmpty(){
+        assertEquals(0, bedroom.countGuests());
+    }
+
+    @Test
+    public void hasRoomNumber() {
+        assertEquals(5, bedroom.getRoomNumber());
+    }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(2, bedroom.getCapacity());
+    }
+
+    @Test
+    public void hasType() {
+        assertEquals("double", bedroom.getType());
+    }
+
 }
