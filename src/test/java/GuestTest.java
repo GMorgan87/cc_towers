6d03c00c40beb1b9e9783369182b10199cca5814
FKeir <fmkeir@gmail.com>
2020-03-25 13:47:12 +0000

    Add tests for guest

--- src/test/java/GuestTest.java
+++ src/test/java/GuestTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuestTest {
+
+    private Guest guest;
+    
+    @Before
+    public void SetUp(){
+        guest = new Guest("Gareth");
+    }
+    
+    @Test
+    public void hasName(){
+        assertEquals("Gareth", guest.getName());
+    }
+
+
 }
